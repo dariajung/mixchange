ec7250f8242c209debf574b210aef997711d5d4a
Daria Jung <djj2115@columbia.edu>
2012-03-25 07:44:50 -0400

    the project set up with hello world at /home

---
+++ music_exchange/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicExchange::Application.config.secret_token = '99f4034753ccca2355d13b1b595e98958e125dc237243c5a710580942a83374663c9fe7aeb35252de5e4e0efd317c5ff9b5d10878760a142a2a8da499171b87a'
