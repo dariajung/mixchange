2b0729ef79ee5277cff71210e822f8ba2a448217
Daria Jung <djj2115@columbia.edu>
2012-04-22 21:37:29 -0400

    Edit user not saving; pushing again just to make sure code is up to date

--- music_exchange/app/models/user.rb
+++ music_exchange/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-	#attr_accessible :email, :name, :surname, :about, :username, :password, :password_digest, :password_confirmation
+	attr_accessible :email, :name, :surname, :about, :username, :password, :password_digest, :password_confirmation
 	has_secure_password
 	validates_presence_of :password, :on => :create
 
